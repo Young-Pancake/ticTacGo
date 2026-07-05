e76ac2af00b4c13115356b757ecd7765c19d21e3
Young-Pancake <salehrammah333@gmail.com>
2026-07-05 09:26:51 +0300

    added return feature

--- functions.go
+++ functions.go
@@ -1,40 +1,45 @@
 package main
-import "fmt"
+import (
+    "fmt"
+    "math"
+)
 
-func drawFrame(body map[int]map[int]string) error {
+func drawFrame(body map[int]map[int]string, drawMode bool, cache map[int][]int, count *int) error {
     template := []rune(`
 
          |       |
-         |       |    
+     1   |   2   |   3
   _______|_______|_______
          |       |
-         |       |    
+     4   |   5   |   6
   _______|_______|_______
          |       |
-         |       |    
+     7   |   8   |   9
          |       |
 `)
 
 	placeHolders := [][]int{[]int{26, 34, 42},
                             []int{94, 102, 110},
                             []int{162, 170, 178}}
-
     for keyRow, rows := range body {
         for keyCol, val := range rows {
-            if val == "X" {
-                template[placeHolders[keyRow][keyCol]] = 'X'
-            } else if val == "O" {
-                template[placeHolders[keyRow][keyCol]] = 'O'
-            } else { return fmt.Errorf("unknown error") }
+                template[placeHolders[keyRow][keyCol]] = []rune(val)[0]
         }
+    }
+    if drawMode {
+        *count++
+    } else {
+        template[placeHolders[rune(cache[*count - 1][0])][rune(cache[*count - 1][1])]] = ' '
+
+        delete(body[cache[*count - 1][0]], cache[*count - 1][1])
+        delete(cache, *count - 1)
+        *count--
     }
 	fmt.Println(string(template))
     return nil
 }
 
-func gameStatus(body map[int]map[int]string, count *int) int {
-
-    *count++
+func gameStatus(body map[int]map[int]string, count int) int {
 
     switch true {
     case body[0][0] == "X" && body[0][1] == "X" && body[0][2] == "X":
@@ -71,9 +76,13 @@ func gameStatus(body map[int]map[int]string, count *int) int {
         return -1
     case body[0][2] == "O" && body[1][1] == "O" && body[2][0] == "O":
         return -1
-    case *count == 9:
+    case count == 9:
         return 2
     default:
         return 0
     }
+}
+
+func caching(cache map[int][]int, pos, count int) {
+    cache[count - 1] = []int{int(math.Floor(float64(pos - 1) / 3.0)), (pos - 1) % 3}
 }
--- main.go
+++ main.go
@@ -4,104 +4,129 @@ import (
     "fmt"
     "bufio"
     "os"
-    "strings"
     "strconv"
+    "math"
+    "strings"
 )
 
 func main() {
-    gameBody := make(map[int]map[int]string)
+    gameBody := make(map[int]map[int]string, 9)
     for i := range 3 {
-        gameBody[i] = make(map[int]string)
+        gameBody[i] = make(map[int]string, 3)
     }
     scanner := bufio.NewScanner(os.Stdin)
     fmt.Println(
 `
-\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-\\\\\\\\|@@@@@@@@@@@|\\\\\\\
-\\\\\\\\|TackTackGo!|\\\\\\\
-\\\\\\\\|@@@@@@@@@@@|\\\\\\\
-\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+ \\\\\\\\\\\\\\\\\\\\\\\\\\
+ \\\\\\\\|         |\\\\\\\
+ \\\\\\\\|TicTacGo!|\\\\\\\
+ \\\\\\\\|         |\\\\\\\
+ \\\\\\\\\\\\\\\\\\\\\\\\\\
 `)
+
     isX := true
     count := 0
     currentChar := "X"
+    cache := make(map[int][]int)
+    initErr := drawFrame(gameBody, true, cache, &count)
+
+    if initErr != nil {
+            fmt.Println("frames failed to generate...Closing")
+            os.Exit(1)
+    }
+
     for {
+        //fmt.Println("currrent cached data:", cache)
         if isX {
-            fmt.Printf("X's turn (row, col)> ")
+            fmt.Printf("\nX's turn> ")
         } else {
-            fmt.Printf("O's turn (row, col)> ")
+            fmt.Printf("\nO's turn> ")
         }
         scanner.Scan()
-        text := strings.Fields(scanner.Text())
 
-        if len(text) != 2 {
-            fmt.Printf("\ninvalid format, please enter again\n")
+        if len( strings.Fields(scanner.Text())) == 0 {
+            fmt.Println("Please enter a non empty value")
+            continue
+        }
+        text := strings.Fields(scanner.Text())[0]
+        pos, _ := strconv.Atoi(text)
+
+        if (text == "return" || text == "back") && count > 1 {
+            rtnErr := drawFrame(gameBody, false, cache, &count)
+            if rtnErr != nil {
+                fmt.Println("invalid return, exiting program")
+                os.Exit(1)
+            }
+            if currentChar == "X" {
+            currentChar = "O"
+            } else { currentChar = "X" }
+            isX = !isX
             continue
         }
 
-        row, _ := strconv.Atoi(text[0])
-        col, _ := strconv.Atoi(text[1])
-
-        if row > 2 || row < 0 || col > 2 || col < 0 {
+        if pos > 9 || pos < 1 {
             fmt.Printf("\ninvalid index, please enter again\n")
             continue
         }
 
         for {
+            row, col := int(math.Floor((float64(pos) - 1.0) / 3.0)), (pos - 1) % 3
             if _, ok := gameBody[row][col]; ok {
                 fmt.Println("invalid position, please enter a different place")
                 if isX {
-                    fmt.Printf("X's turn (row, col)> ")
+                    fmt.Printf("X's turn> ")
                 } else {
-                    fmt.Printf("O's turn (row, col)> ")
+                    fmt.Printf("O's turn> ")
                 }
                 scanner.Scan()
-                text = strings.Fields(scanner.Text())
-                row, _ = strconv.Atoi(text[0])
-                col, _ = strconv.Atoi(text[1])
+                text = scanner.Text()
+                pos, _ = strconv.Atoi(text)
+                if pos > 9 || pos < 1 {
+                    continue
+                }
             } else {
                 gameBody[row][col] = currentChar
                 break
             }
         }
-        err := drawFrame(gameBody)
+        err := drawFrame(gameBody, true, cache, &count)
         if err != nil {
             fmt.Println("frames failed to generate...Closing")
             os.Exit(1)
         }
-        fmt.Printf("\nEntered:\nrow: %d\ncol: %d\n", row, col)
         if currentChar == "X" {
             currentChar = "O"
         } else { currentChar = "X" }
+
         isX = !isX
-        status := gameStatus(gameBody, &count)
+        status := gameStatus(gameBody, count - 1)
+        caching(cache, pos, count)
         if status != 0 {
             if status == 1 {
-                fmt.Println(`
-                    \\\\\\\\\\\\\\\\\\\\\\\\\\\\
-                    \\\\\\\\|@@@@@@@@@@@|\\\\\\\
-                    \\\\\\\\|   X WON!  |\\\\\\\
-                    \\\\\\\\|@@@@@@@@@@@|\\\\\\\
-                    \\\\\\\\\\\\\\\\\\\\\\\\\\\\
-                    `)
+               fmt.Println(`
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+\\\\\\\\|           |\\\\\\\
+\\\\\\\\|   X WON!  |\\\\\\\
+\\\\\\\\|           |\\\\\\\
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+`)
             } else if status == -1 {
-                fmt.Println(
-                    `
-                    \\\\\\\\\\\\\\\\\\\\\\\\\\\\
-                    \\\\\\\\|@@@@@@@@@@@|\\\\\\\
-                    \\\\\\\\|   O WON!  |\\\\\\\
-                    \\\\\\\\|@@@@@@@@@@@|\\\\\\\
-                    \\\\\\\\\\\\\\\\\\\\\\\\\\\\
-                    `)
+               fmt.Println(`
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+\\\\\\\\|           |\\\\\\\
+\\\\\\\\|   O WON!  |\\\\\\\
+\\\\\\\\|           |\\\\\\\
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+`)
             } else if status == 2 {
-                fmt.Println(
-                    `
-                    \\\\\\\\\\\\\\\\\\\\\\\\\\\\
-                    \\\\\\\\|@@@@@@@@@@@|\\\\\\\
-                    \\\\\\\\|   DRAW!   |\\\\\\\
-                    \\\\\\\\|@@@@@@@@@@@|\\\\\\\
-                    \\\\\\\\\\\\\\\\\\\\\\\\\\\\
-                    `)
+               fmt.Println(`
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+\\\\\\\\|           |\\\\\\\
+\\\\\\\\|   DRAW!   |\\\\\\\
+\\\\\\\\|           |\\\\\\\
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+`)
             }
             os.Exit(1)
         }
